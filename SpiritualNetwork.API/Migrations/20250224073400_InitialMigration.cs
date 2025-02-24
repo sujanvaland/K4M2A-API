646f77be1665dc8323a2208bed598dc0e73182a4
Sujan <sujanvaland@gmail.com>
2025-02-24 17:17:19 +0530

    a

---
+++ SpiritualNetwork.API/Migrations/20250224073400_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpiritualNetwork.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrincipal",
+                schema: "dbo",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WebsiteLink",
+                schema: "dbo",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrincipal",
+                schema: "dbo",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "WebsiteLink",
+                schema: "dbo",
+                table: "Users");
+        }
+    }
+}
